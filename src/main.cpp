746588a380722df7fbf0ec87df323fb205475c35
Miles Conrad <milesconrad@pm.me>
2021-09-29 15:37:28 -0400

    added function prototypes

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,46 @@
 // saves a lot of space in the GetTime() function
 using namespace std::chrono;
 
-double GetTime() {
-    double ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    return ms;
+double GetTime();
+void HandleEvent(sf::Event event, Game *game, bool (*keys)[2]);
+void ScoreKeeper(Game *game);
+
+int main() {
+    Game game;
+    game.init(GetTime());
+    std::thread ScoreKeeperThread(ScoreKeeper, &game);
+
+    // PressedKeys[0] is left arrow, PressedKeys[1] is right arrow
+    bool PressedKeys[2] = {false, false};
+    double LastFrame = game.StartTime;
+    float DeltaTime;
+    while (game.window.isOpen()) {
+        sf::Event event;
+        if (game.window.pollEvent(event)) {
+            HandleEvent(event, &game, &PressedKeys);
+        }
+
+        DeltaTime = (GetTime() - LastFrame) / 1000;
+        LastFrame = GetTime();
+
+        if (game.CollisionDetect()) {
+            game.end();
+        }
+
+        game.run(DeltaTime, &PressedKeys);
+        game.window.display();
+    }
+
+    ScoreKeeperThread.join();
+    return 0;
+}
+
+void ScoreKeeper(Game *game) {
+    while ((*game).window.isOpen()) {
+        if ((*game).GameRunning) {
+            (*game).score = (GetTime() - (*game).StartTime) / 1000;
+        }
+    } 
 }
 
 void HandleEvent(sf::Event event, Game *game, bool (*keys)[2]) {
@@ -39,40 +76,7 @@ void HandleEvent(sf::Event event, Game *game, bool (*keys)[2]) {
     }
 }
 
-void ScoreKeeper(Game *game) {
-    while ((*game).window.isOpen()) {
-        if ((*game).GameRunning) {
-            (*game).score = (GetTime() - (*game).StartTime) / 1000;
-        }
-    } 
-};
-
-int main() {
-    Game game;
-    game.init(GetTime());
-    std::thread ScoreKeeperThread(ScoreKeeper, &game);
-
-    // PressedKeys[0] is left arrow, PressedKeys[1] is right arrow
-    bool PressedKeys[2] = {false, false};
-    double LastFrame = game.StartTime;
-    float DeltaTime;
-    while (game.window.isOpen()) {
-        sf::Event event;
-        if (game.window.pollEvent(event)) {
-            HandleEvent(event, &game, &PressedKeys);
-        }
-
-        DeltaTime = (GetTime() - LastFrame) / 1000;
-        LastFrame = GetTime();
-
-        if (game.CollisionDetect()) {
-            game.end();
-        }
-
-        game.run(DeltaTime, &PressedKeys);
-        game.window.display();
-    }
-
-    ScoreKeeperThread.join();
-    return 0;
+double GetTime() {
+    double ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    return ms;
 }
